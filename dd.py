75682594e5a1d7ba53ae39392062d332d6078076
anujpandey <ap9089511@gmail.com>
2025-06-17 20:40:34 +0530

    chnge

---
+++ dd.py
@@ -0,0 +1,10 @@
+name = "aanuj pandey"
+friend= 'priyanshu pandey'
+print(name[2])
+print(" lets use a for loop\n")
+
+for character in name:
+    print(character)
+    
+    for character in friend:
+        print(character)
